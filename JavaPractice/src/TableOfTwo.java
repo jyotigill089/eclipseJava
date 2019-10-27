5d2c934f16b4b4cd77ddf5483175b8966002c124
Jyoti Gill <jyotigill089@gmail.com>
2019-10-27 11:52:34 -0700

    Added program of table of 2

---
+++ JavaPractice/src/TableOfTwo.java
@@ -0,0 +1,14 @@
+
+public class TableOfTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Following is the table of 2");
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println("2 x "+i+" = "+i*2);
+		}
+
+	}
+
+}
